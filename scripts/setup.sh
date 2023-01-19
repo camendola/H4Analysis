6a125818b0ea82196dc9e90dc64ce3f1ce8cf246
chiara amendola <chiara.amendola@cern.ch>
2023-01-19 17:27:18 +0100

    clean up batch scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-eval "$(conda shell.bash hook)"
-conda activate /eos/project/c/cms-ecal-calibration/ecal-conda/h4analysis
+#eval "$(conda shell.bash hook)"
+#conda activate /eos/project/c/cms-ecal-calibration/ecal-conda/h4analysis
+source /cvmfs/sft.cern.ch/lcg/views/LCG_100/x86_64-centos7-gcc8-opt/setup.sh
 
 export LD_LIBRARY_PATH=./lib:DynamicTTree/lib/:CfgManager/lib/:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=./interface:DynamicTTree/interface/:CfgManager/interface/:$ROOT_INCLUDE_PATH
